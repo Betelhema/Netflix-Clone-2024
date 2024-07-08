1b75a2c6e6848ba89842fc2da0560cc07a8c8fe1
betelhema <111201258+betelhemayalew@users.noreply.github.com>
2024-07-08 21:05:46 +0300

    header and footer

--- netflix-app/src/components/Header/Header.jsx
+++ netflix-app/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png";
-// import SearchIcon from "@mui/icons-material/Search";
-// import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-// import AccountBoxIcon from "@mui/icons-material/AccountBox";
-// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import SearchIcon from '@mui/icons-material/Search';
+
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Header = () => {
   return (
@@ -25,7 +26,7 @@ const Header = () => {
         </div>
         <div className="header_right">
           <ul>
-            {/* <li>
+            <li>
               <SearchIcon />
             </li>
             <li>
@@ -36,7 +37,7 @@ const Header = () => {
             </li>
             <li>
               <ArrowDropDownIcon />
-            </li> */}
+            </li>
           </ul>
         </div>
       </div>
