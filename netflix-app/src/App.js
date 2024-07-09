d3b9c7c33dfb9197a830781f38d80569c063852e
betelhema <111201258+betelhemayalew@users.noreply.github.com>
2024-07-10 01:36:37 +0300

    banner

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <>
-<Header/>
-<Footer/>
-    </>
+    <div className="App">
+    <Home/>
+    </div>
   );
 }
 
