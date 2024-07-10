163698e9a1b8928239b417d62a59f52f2625720c
betelhema <111201258+betelhemayalew@users.noreply.github.com>
2024-07-10 23:35:33 +0300

    row

--- netflix-app/src/pages/Home/Home.jsx
+++ netflix-app/src/pages/Home/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
-// import RowList from '../../components/'
+import RowList from '../../components/Rows/Rowlist/Rowlist'
+
 
 const Home = () => {
     return (
         <>
             <Header />
             <Banner />
-            {/* <RowList /> */}
+           <RowList/>
             <Footer />
         </>
     )
